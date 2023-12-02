f786f075680d02faa364c4dc2c5c1e9fd17d3f8c
Tyler Gallant <tylergallant@live.com>
2023-12-02 16:25:57 -0500

    feat: day 1 puzzle 1

--- src/Day01.hs
+++ src/Day01.hs
@@ -1,10 +1,16 @@
 module Day01 where
 
+import Data.Char (isDigit)
 import Paths_aoc2023 (getDataFileName)
+
+solution1 :: String -> Int
+solution1 = sum . fmap calibrationValue . lines
+  where
+    calibrationValue = read . firstAndLast . filter isDigit
+    firstAndLast s = head s : [last s]
 
 day01 :: IO ()
 day01 = do
-  inputLines <- lines <$> (getDataFileName "day01-input.txt" >>= readFile)
-  putStrLn "This is what I read from input:"
-  putStrLn $ unlines inputLines
-  putStrLn "TODO: implement Day 01"
+  input <- getDataFileName "day01-input.txt" >>= readFile
+  let answer1 = solution1 input
+  putStrLn $ "Puzzle 1: " ++ show answer1
